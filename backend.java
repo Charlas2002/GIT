7713b76eb2b64ff8e83e27d11c7dece5eb989012
charlaskunanidhi2002@gmail.com <charlaskunanidhi2002@gmail.com>
2025-01-11 11:22:37 +0530

    fix : changes done on database class

--- backend.java
+++ backend.java
@@ -1,12 +1,11 @@
 class Database {
-    void base() {
-        String data = "I'm using MySQL";
+    void base(String data) {
         System.out.println(data);
     }
 }
 public class backend {
     public static void main(String[] args) {
         Database obj = new Database();
-        obj.base(); 
+        obj.base("MySQL"); 
     }
 }
